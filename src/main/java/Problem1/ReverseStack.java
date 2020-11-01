4ab2eeb906cc46d22e5b783495dd67f10cc44b00
Rachel Kim <rachelkim8084@gmail.com>
2020-11-01 01:21:03 -0800

    ReverseStack done.

--- src/main/java/Problem1/ReverseStack.java
+++ src/main/java/Problem1/ReverseStack.java
@@ -3,7 +3,25 @@ package Problem1;
 import java.util.Stack;
 
 public class ReverseStack {
+    //Pop all elements out of the stack
     public static void reverseStack(Stack<Integer> stack) {
-        // homework
+        if (stack != null && stack.size() != 0) { // check if stack is null and not empty
+            int x = stack.pop();
+            reverseStack(stack);
+            insert(stack, x);
+        }
+    }
+
+    //helper function
+    //insert elements back in reverse order
+    //int x: element to insert
+    public static void insert(Stack<Integer> stack, int x) {
+        if (stack.size() == 0) {
+            stack.push(x);
+        } else {
+            int a = stack.pop();
+            insert(stack, x);
+            stack.push(a);
+        }
     }
 }
