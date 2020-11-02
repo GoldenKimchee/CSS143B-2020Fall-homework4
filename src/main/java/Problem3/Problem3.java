5462e2950cc56061cd8cee0afad6957a83fbbbd1
Rachel Kim <rachelkim8084@gmail.com>
2020-11-01 22:09:36 -0800

    Problem 3 done. Nice break from the hard stuff.

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -6,6 +6,15 @@ import java.io.PrintStream;
 
 public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
-        // homework
+        if (list == null) { //if no items in list
+            return;
+        }
+
+        //main function
+        //recurse till at the end of the list
+        if (list.next != null) {
+            printListInReverse(list.next, print);
+        }
+        print.println(list.val); //finally, print items in reverse order
     }
 }
