9caaa3c6261c6f33873feabc884e9d0b7c78037e
Rachel Kim <rachelkim8084@gmail.com>
2020-11-03 22:53:39 -0800

    Finished permutation. Only sort list problem left.

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -1,18 +1,25 @@
 package Problem1;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        permute(nums, 0, result);
+        if (nums == null) {
+            return null;
+        } else if (nums.length == 0) {
+            List<Integer> one = new ArrayList<>();
+            result.add(one);
+        } else {
+            permute(nums, 0, result);
+        }
         return result;
     }
 
     //[1,2,3]
     //pick every number and put it in the front
-
     private static void permute(int[] nums, int position, List<List<Integer>> result) {
         //Stopping condition
         if (position == nums.length - 1) {
@@ -20,21 +27,23 @@ public class Permutation {
             for (int i = 0; i < nums.length; i++) {
                 oneResult.add(nums[i]);
             }
+            result.add(oneResult);
             return;
+        } else {
+            for (int i = position; i < nums.length; i++) {
+                swap(nums, i, position);
+                permute(nums, position + 1, result);
+                swap(nums, i, position);
+            }
         }
-
-        permute(nums, position + 1, result);
         //put number at position in the front
         //call the same private permute with position + 1
         //put the number back to its original position
     }
 
-
-    public static void main(String[] args) {
-        ArrayList items = new ArrayList();
-        items.add("Rachel");
-        items.add('h');
-        items.add(8);
-        System.out.println(items);
+    private static void swap(int[] data, int i, int j) {
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
     }
 }
