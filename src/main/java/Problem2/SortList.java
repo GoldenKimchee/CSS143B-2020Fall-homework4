42eee5bb05dc9edc8fbd004d7364a69919f68457
Rachel Kim <rachelkim8084@gmail.com>
2020-11-05 12:38:29 -0800

    Saving progress for now.

--- src/main/java/Problem2/SortList.java
+++ src/main/java/Problem2/SortList.java
@@ -17,56 +17,56 @@ public class SortList {
         return mergeLists(sortList(head), sortList(mid)); //split into halves until element by itself and put together
     }
 
+
+    //makes two passes
     public static ListNode findMidAndBreak(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
         int length = 0;
         ListNode temp = head;
-        while (temp != null) {
+        while (temp != null) { //go through list to count length
             length++;
             temp = temp.next;
         }
-        temp = head;
-        length = length / 2;
-        int iteration = 0;
+
+        temp = head; //reset to point to head
+        length = length / 2; //find middle
+        int iteration = 1;
         while (iteration < length) {
             iteration++;
             temp = temp.next;
         }
-        ListNode address = temp.next;
-        temp.next = null;
-        return address; //output should be pointing (a reference) to the middle node
+        ListNode address = temp.next; //store address of next node
+        temp.next = null; //have the current node (at halfway point) now point to null (splits list in half)
+        return address; //reference to the middle node
     }
 
 
     public static ListNode mergeLists(ListNode list1, ListNode list2) {
         ListNode temp1 = list1;
         ListNode temp2 = list2;
-        ListNode merged;
-        // 1, 3, 4
-        // 2, 4, 5
-        if (temp1.val < temp2.val) {
-            merged = temp1;
-            temp1 = temp1.next;
-        } else { //if temp1.val > temp2.val || temp1.val == temp2.val
-            merged = temp2;
-            temp2 = temp2.next;
-        }
-
-        while (temp1 != null  && temp2 != null) { //both have to be empty
+        ListNode merged = null;
+        //
+        //
+        while (temp1 != null || temp2 != null) { //both have to be empty
             if (temp1.val < temp2.val) {
-                merged.next = temp1;
+                merged = temp1;
                 temp1 = temp1.next;
             } else if (temp1.val > temp2.val || temp1.val == temp2.val) {
-                merged.next = temp2;
+                merged = temp2;
                 temp2 = temp2.next;
             } else if (temp1 == null) {
                 while (temp2 != null) {
-                    merged.next = temp2;
+                    merged = temp2;
                     temp2 = temp2.next;
+                    merged = merged.next;
                 }
             } else if (temp2 == null) {
                 while (temp1 != null) {
-                    merged.next = temp1;
+                    merged = temp1;
                     temp1 = temp1.next;
+                    merged = merged.next;
                 }
             }
             merged = merged.next;
